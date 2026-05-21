d8c531b17305f097cf476c490a8a45b27b614864
shlok-git340 <shlokbhutani6@gmail.com>
2026-05-21 07:02:53 +0000

    switch to drf spectacular for api doc

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'rest_framework_simplejwt',
     'corsheaders',
     'drf_yasg',
+    'drf_spectacular',
     'channels',
     'users',
     'chat',
@@ -139,4 +140,5 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
 }
--- backend/config/urls.py
+++ backend/config/urls.py
@@ -1,20 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
 
-from rest_framework import permissions
-
-from drf_yasg.views import get_schema_view
-from drf_yasg import openapi
-
-
-schema_view = get_schema_view(
-    openapi.Info(
-        title="Realtime Chat API",
-        default_version='v1',
-        description="Realtime chat backend with Django Channels",
-    ),
-    public=True,
-    permission_classes=[permissions.AllowAny],
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularSwaggerView,
 )
 
 urlpatterns = [
@@ -25,11 +14,16 @@ urlpatterns = [
     path('api/chat/', include('chat.urls')),
 
     path(
-        'swagger/',
-        schema_view.with_ui('swagger', cache_timeout=0),
+        'api/schema/',
+        SpectacularAPIView.as_view(),
+        name='schema',
     ),
+
     path(
-    'redoc/',
-    schema_view.with_ui('redoc', cache_timeout=0),
-),
+        'swagger/',
+        SpectacularSwaggerView.as_view(
+            url_name='schema'
+        ),
+        name='swagger-ui',
+    ),
 ]
